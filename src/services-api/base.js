0183e7531b12549f87e92f2967add2f6f27a2dd9
factorgh <burchellsbale@gmail.com>
2024-12-19 13:36:20 +0000

    Latest update

--- src/services-api/base.js
+++ src/services-api/base.js
@@ -4,7 +4,7 @@ const PUBLIC_URL = import.meta.env.VITE_API_URL;
 
 // Create a base API client with Vite's public URL and JSON content-type header.
 const axiosInstance = axios.create({
-  baseURL: PUBLIC_URL,
+  baseURL: `${PUBLIC_URL}/api/v1`,
   headers: {
     "Content-Type": "application/json",
   },
